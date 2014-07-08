188bd24faa3e8ef0f1a26e77f728119e1dc8ff24
Connor McCann <kb1udn@gmail.com>
2014-07-08 16:36:24 -0400

    fixed minor descrepancies between the left and right sides of the pigeon so as to reduce sidways tumbling

--- pigeon_8/robots/runFlapping.m
+++ pigeon_8/robots/runFlapping.m
@@ -11,14 +11,14 @@ p = compile(p);
 x0 = zeros(42,1); % initial state
 x0(3) = 5; % initial height
 x0(22) = 20; % initial forward velocity
-x0(5) = .2; % initial pitch
+x0(5) = -.2; % initial pitch
 
 u = zeros(15,pts);
 u(1,:) = sin(linspace(0,tf,pts));
-u(5,:) = sin(linspace(0,10*tf,pts));
-u(8,:) = .7*sin(linspace(0,10*tf,pts));
-u(12,:) = -sin(linspace(0,10*tf,pts));
-u(15,:) = -.7*sin(linspace(0,10*tf,pts));
+u(5,:) = -sin(linspace(0,10*tf,pts));
+u(8,:) = -.7*sin(linspace(0,10*tf,pts));
+u(12,:) = sin(linspace(0,10*tf,pts));
+u(15,:) = .7*sin(linspace(0,10*tf,pts));
 utraj = PPTrajectory(foh(linspace(0,tf,pts),u));
 utraj = setOutputFrame(utraj, p.getInputFrame);
 
