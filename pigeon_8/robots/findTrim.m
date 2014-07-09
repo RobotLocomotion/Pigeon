a8975d9d906f9a0c1acaa5e6301a746a3bbd08f1
Benoit Landry <landry@mit.edu>
2014-07-08 23:09:21 -0700

    trim conditions script

--- pigeon_8/robots/findTrim.m
+++ pigeon_8/robots/findTrim.m
@@ -1,7 +1,7 @@
 function [ xsol,usol,exitflag ] = findTrim( )
 
 options.floating = true;
-p = RigidBodyManipulator('pigeon_2.URDF', options);
+p = RigidBodyManipulator('pigeon_8.URDF', options);
 
 x0 = zeros(42,1);
 x0(1) = 5;
@@ -12,9 +12,9 @@ options.MaxFunEvals = 10000;
 options.Algorithm = 'active-set';
 A = zeros(57);
 b = zeros(57,1);
-tol = 1E-2;
-lb = [1 1 1 -1.5 -1.5 -1.5 -1.5*ones(1,15) 5 0 -tol -tol -tol -tol zeros(1,15) -Inf*ones(1,15)]';
-ub = [1 1 1 1.5 1.5 1.5 1.5*ones(1,15) 50 0 tol tol tol tol zeros(1,15) Inf*ones(1,15)]';
+tol = 1E-4;
+lb = [1 1 1 -1.5 -1.5 -1.5 -1.5*ones(1,15) 40 0 -tol 0 0 0 zeros(1,15) -Inf*ones(1,15)]';
+ub = [1 1 1 1.5 1.5 1.5 1.5*ones(1,15) 50 0 tol 0 0 0 zeros(1,15) Inf*ones(1,15)]';
 [sol,~,exitflag] = fmincon(@minfun, X, A, b, [], [], lb, ub, [], options);
     
 function y = minfun(inX)
@@ -22,8 +22,8 @@ function y = minfun(inX)
     u = inX(43:57);
     xdot = p.dynamics(0,x,u);
     y = xdot(4:6)'*xdot(4:6);
-    y = y+xdot(3)^2;
-    y = y+xdot(24)^2;
+    y = y+100*xdot(3)^2;
+    y = y+100*xdot(24)^2;
 end
 
 xsol = sol(1:42);
@@ -34,7 +34,7 @@ dt = .01;
 pts = tf/dt;
 
 options.floating = true;
-p = TimeSteppingRigidBodyManipulator('pigeon_2.URDF',dt,options);
+p = TimeSteppingRigidBodyManipulator('pigeon_8.URDF',dt,options);
 p = enableIdealizedPositionControl(p,true);
 p = compile(p);
 
