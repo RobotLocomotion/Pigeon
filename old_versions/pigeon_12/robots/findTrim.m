3ef4fa75cdaf8f73f3f7fe75a2f04b2a1e2934f6
Benoit Landry <landry@mit.edu>
2014-07-28 21:11:22 -0700

    dircol flapping for model 12

--- old_versions/pigeon_12/robots/findTrim.m
+++ old_versions/pigeon_12/robots/findTrim.m
@@ -1,7 +1,9 @@
-function [ xstar,ustar ] = findTrim( )
+function [xstar, ustar] = findTrim(p)
 
-options.floating = true;
-p = RigidBodyManipulator('pigeon_12.URDF', options);
+if (nargin<1)
+  options.floating = true;
+  p = RigidBodyManipulator('pigeon_12.URDF', options);
+end
 
 prog = FixedPointProgram(p,[1;3]);  % ignores xdot and zdot constraints
 
@@ -12,8 +14,8 @@ ub = [0 0 5 1.5 0 1.5 1.5*ones(1,16) ...
     30 0 0 0 0 0 zeros(1,16) ...
     Inf*ones(1,16)]';
 
-prog = addBoundingBoxConstraint(prog,BoundingBoxConstraint(lb,ub));
-prog = addDifferentiableConstraint(prog,FunctionHandleObjective(60,@cost));
+prog = addConstraint(prog,BoundingBoxConstraint(lb,ub));
+prog = addConstraint(prog,FunctionHandleObjective(60,@cost));
 
 x0 = Point(getStateFrame(p),zeros(44,1));
 x0.base_xdot = 20;
@@ -24,12 +26,14 @@ w = prog.solve([double(x0);u0]);
 xstar = w(1:44);
 ustar = w(45:end);
 
-utraj = ConstantTrajectory(ustar);
-utraj = setOutputFrame(utraj,getInputFrame(p));
-sys = cascade(utraj,p);
-xtraj = simulate(sys,[0 .5],xstar);
-v = p.constructVisualizer(); 
-v.playback(xtraj, struct('slider',true));
+if (nargout<1)
+  utraj = ConstantTrajectory(ustar);
+  utraj = setOutputFrame(utraj,getInputFrame(p));
+  sys = cascade(utraj,p);
+  xtraj = simulate(sys,[0 .1],xstar);
+  v = p.constructVisualizer(); 
+  v.playback(xtraj, struct('slider',true));
+end
 
 end
 
@@ -37,5 +41,5 @@ function [c,dc] = cost(x)
     xdot = x(23);
     zdot = x(25);
     c = zdot/xdot;
-    dc = [zeros(1,23) -zdot/xdot^2 0 1/xdot zeros(1,18) zeros(1,16)];
+    dc = [zeros(1,22) -zdot/xdot^2 0 1/xdot zeros(1,19) zeros(1,16)];
 end
